084bc21337f5304a91b0c57da7dac429d17cbe0b
ramiz14 <ramizng@code.edu.az>
2022-11-18 17:53:08 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+let regex=new RegExp()
+regex=/(?=^.{8,}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s)[0-9a-zA-Z!@#$%^&*()]*$/
+let regexemail=/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+let upper=/^[A-Z][a-z0-9_-]{5,19}$/
+let usr=document.querySelector('#usr')
+let psw=document.querySelector('#psw')
+let email=document.querySelector('#email')
+var btn=document.getElementById('btn')
+btn.onclick=function logn() {
+    
+    if (!regex.test(psw.value)) {
+        psw.style.border='2px solid red'
+    }
+    else{
+        psw.style.border=`2px solid #ebebeb`
+    }
+    if (!upper.test(usr.value)) {
+        usr.style.border='2px solid red'
+    }
+    else{
+        usr.style.border=`2px solid #ebebeb`
+    }
+    if (!regexemail.test(email.value)) {
+        email.style.border='2px solid red'
+    }
+    else{
+        email.style.border=`2px solid #ebebeb`
+    }
+    setTimeout(() => {
+        if (regex.test(psw.value)&&upper.test(usr.value)&&regexemail.test(email.value)) {
+            location.reload()
+            alert('logged in succesfully')
+        }
+    }, 100);
+}
